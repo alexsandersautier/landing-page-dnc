c846fcf84ea5795f065741d6d32536c2c73278dd
alexsandersautier <alexsanderesramos@gmail.com>
2023-05-14 20:31:55 -0300

    site dnc

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function rolaParaDireita(){
     leonardo.style = 'display:none;'
     bruna.style = 'display:flex;'
     seta_direita.style = 'display:none;'
-    seta_esquerda.style = 'display:flex; margin-top:150%;'
+    seta_esquerda.style = 'display:flex; margin-top:90%; margin-right: 25px;'
 }
 function rolaParaEsquerda(){
     leonardo.style = 'display:flex;'
     bruna.style = 'display:none;'
-    seta_direita.style = 'display:flex;'
-    seta_esquerda.style = 'display:none; margin-top:150%;'
+    seta_direita.style = 'display:flex;margin-top:100%;'
+    seta_esquerda.style = 'display:none; '
 }
